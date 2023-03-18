38efc3296b2150ef3fd720d839f01f415d008513
nickqq11 <nickaids@gmail.com>
2023-03-18 20:31:49 +1030

    linft and makefile

--- Makefile
+++ Makefile
@@ -6,7 +6,25 @@
 #    By: nhuang <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/03/06 17:53:07 by nhuang            #+#    #+#              #
-#    Updated: 2023/03/06 17:53:37 by nhuang           ###   ########.fr        #
+#    Updated: 2023/03/16 17:49:51 by nhuang           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
+NAME = libft.a
+
+CC = gcc -Wall -Wextra -Werror 
+
+
+
+OBJECTS = 
+
+%.o : %.c 
+	@$(CC) $< -o $@
+
+.PHONY : clean fclean
+
+clean :
+	@rm -f *.o
+
+fclean :
+	@rm -f $(NAME)
--- libft.h
+++ libft.h
@@ -6,6 +6,36 @@
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 17:54:15 by nhuang            #+#    #+#             */
-/*   Updated: 2023/03/07 11:13:29 by nhuang           ###   ########.fr       */
+/*   Updated: 2023/03/18 20:29:38 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include<unistd.h>
+#include<stdio.h>
+
+typedef struct s_list
+{
+void *content;
+struct s_list *next;
+} t_list;
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+void	*ft_memset(void *b, int c, size_t len);
+void	ft_bzero(void *s, size_t n);
+void	ft_memcpy(void *dst, const void *src, size_t n);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t ft_strlcat(char *dst, const char *src, size_t dstsize);
+int ft_toupper(int c);
+int ft_tolower(int c);
+char *ft_strchr(const char *s, int c);
+
+
+#endif
