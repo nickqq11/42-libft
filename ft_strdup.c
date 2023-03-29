0fc460fdc7104b88ebc66e418e973b4d35462171
Nickqq11 <nickaids@gmail.com>
2023-03-30 00:00:36 +1030

    calloc

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/21 17:32:51 by nhuang            #+#    #+#             */
-/*   Updated: 2023/03/29 23:56:59 by nhuang           ###   ########.fr       */
+/*   Created: 2023/03/29 23:57:37 by nhuang            #+#    #+#             */
+/*   Updated: 2023/03/30 00:00:36 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"libft.h"
-
-void	*ft_calloc(size_t count, size_t size)
+char    *ft_strdup(const char *s1)
 {
-	size_t	toto;
-	void	*dst;
-
-	toto = size * count;
-	dst = malloc(toto);
-	if (!dst)
-		return (NULL);
-	ft_bzero(dst, toto);
-	return (dst);
+    
 }
