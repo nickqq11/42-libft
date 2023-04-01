1658e2fe358b33cffd399232707682b35511be3c
nickqq11 <nickaids@gmail.com>
2023-04-01 17:58:15 +1030

    working on substr

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 18:10:12 by nhuang            #+#    #+#             */
-/*   Updated: 2023/04/01 13:35:41 by nhuang           ###   ########.fr       */
+/*   Updated: 2023/04/01 17:24:03 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,18 @@ int	ft_atoi(const char *str)
 	int	sum;
 	int	neg;
 
-i = 0;
-sum = 0;
-neg = 1;
-while (str[i] != '\0' && (str[i] == '\t' || str[i] == '\n' || str[i] == '\r' || str[i] =='\v' || str[i] == ' ' || str[i] == '\f'))
+	i = 0;
+	sum = 0;
+	neg = 1;
+	while (str[i] != '\0' && (str[i] == '\t' || str[i] == '\n' || str[i] == '\r'
+			|| str[i] == '\v' || str[i] == ' ' || str[i] == '\f'))
 	i++;
 	if (str[i] == '-')
 	{
 		neg = -1;
 		i++;
 	}
-	if (str[i] == '+')
+	else if (str[i] == '+')
 		i++;
 	while (str[i] != '\0' && ft_isdigit(str[i]))
 	{
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,19 @@
 /*   By: nhuang <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 22:10:41 by nhuang            #+#    #+#             */
-/*   Updated: 2023/03/28 22:10:43 by nhuang           ###   ########.fr       */
+/*   Updated: 2023/04/01 17:55:13 by nhuang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include"libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str || !s)
+		return (0);
+	str = ft_strlcpy(str, (s + start), len);
+	return (str);
+}
